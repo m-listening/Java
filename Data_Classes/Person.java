f0b37078f986c8e98b2dc89c84bff943b1e16325
whoopy.op <whoopy.op@gmail.com>
2023-03-10 20:44:32 +0200

    created class Person

---
+++ Data_Classes/Person.java
@@ -0,0 +1,39 @@
+package Data_Classes;
+
+public class Person {
+
+    private String name = "";
+    private Integer age = 0;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public Person(int age) {
+        this.age = age;
+    }
+
+    public Person() {
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
